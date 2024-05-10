6286e55b8cfcba0c5fbbc9d5a4695873fc119431
marrenata <83846339+marrenata@users.noreply.github.com>
2024-05-10 21:01:50 +0300

    Update FunctionTest.java

--- src/test/java/org/example/FunctionTest.java
+++ src/test/java/org/example/FunctionTest.java
@@ -1,9 +1,26 @@
 package org.example;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Random;
 
 public class FunctionTest {
+
+    private static final int[] testDataSizes = {1000, 10000, 100000};
+    private static int[][] testData;
+
+    @BeforeClass
+    public static void setup() {
+        testData = new int[testDataSizes.length][];
+        Random random = new Random();
+        for (int i = 0; i < testDataSizes.length; i++) {
+            testData[i] = new int[testDataSizes[i]];
+            for (int j = 0; j < testDataSizes[i]; j++) {
+                testData[i][j] = random.nextInt();
+            }
+        }
+    }
 
     @Test
     public void testMin() {
@@ -27,5 +44,31 @@ public class FunctionTest {
     public void testMult() {
         int[] numbers = {5, 8, 3, 12, 7};
         assertEquals(10080, Function._mult(numbers));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        int[] emptyArray = {};
+        try {
+            Function._min(emptyArray);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            // Test passes if this exception is thrown
+        }
+    }
+
+    @Test
+    public void testPerformance() {
+        for (int i = 0; i < testData.length; i++) {
+            long startTime = System.nanoTime();
+            Function._min(testData[i]);
+            Function._max(testData[i]);
+            Function._sum(testData[i]);
+            Function._mult(testData[i]);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            System.out.println("Test data size: " + testDataSizes[i] + ", Time taken (ns): " + duration);
+        }
     }
 }
